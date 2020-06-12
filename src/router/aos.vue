baaf7027bec7425806db55aa993e1c5464764cdf
BSAA7567 <33346331+BSAA7567@users.noreply.github.com>
2020-06-12 18:07:09 +0900

    Update aos.vue

--- src/router/aos.vue
+++ src/router/aos.vue
@@ -1,20 +1,20 @@
 <template>
-  <div>
-    <div data-aos='fade-down'>
+  <article>
+    <section data-aos='fade-down'>
       <img src="../assets/a.png" alt="a img">
       <p>a img</p>
-    </div>
-    <div data-aos='fade-down'>
+    </section>
+    <section data-aos='fade-down'>
       <img src="../assets/b.png" alt="b img">
       <p>b img</p>
-    </div>
-    <div data-aos='fade-down'>
+    </section>
+    <section data-aos='fade-down'>
       <img src="../assets/c.png" alt="c img">
       <p>c img</p>
-    </div>
-    <div data-aos='fade-down'>
+    </section>
+    <section data-aos='fade-down'>
       <img src="../assets/d.png" alt="d img">
       <p>d img</p>
-    </div>
-  </div>
+    </section>
+  </article>
 </template>
